e85064211f8d721194e2c2dbf5cce2de19769377
AlexGronskiy <85637138+AlexGronskiy@users.noreply.github.com>
2022-10-16 10:23:36 +0300

    Add files via upload

---
+++ ДЗ 3. Flask/main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/music_list/', methods=['get'])
+def all_tracks():
+    with sqlite3.connect('music_lib.sqlite3') as connection:
+        cursor = connection.cursor()
+        music = cursor.execute(
+            "SELECT music.id, music.name, author.name, genre.name FROM music JOIN author ON music.author_id = "
+            "author.id JOIN genre ON music.genre_id = genre.id")
+        context = {'musics': music.fetchall()}
+        return render_template("music_list.html", **context)
+
+
+@app.route('/add_music/', methods=['get', 'post'])
+def add_tracks():
+
+    context = {'genres': [], 'authors': []}
+    with sqlite3.connect('music_lib.sqlite3') as connection:
+        cursor = connection.cursor()
+        genres = cursor.execute("SELECT id, name FROM genre")
+        context['genres'] = genres.fetchall()
+        authors = cursor.execute("SELECT id, name FROM author")
+        context['authors'] = authors.fetchall()
+
+    if request.method == 'POST':
+
+        with sqlite3.connect('music_lib.sqlite3') as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                "INSERT INTO music(name, author_id, genre_id) VALUES ("
+                f"'{request.form['name']}',"
+                f" '{request.form['author']}',"
+                f" '{request.form['genre']}')"
+            )
+            connection.commit()
+
+    return render_template('add_music.html', **context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
